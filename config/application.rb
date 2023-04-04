c715a977654dcaf2e3a4d3d62c45452ec6b9d8a3
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-04-04 03:25:59 -0700

    Add linter

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,11 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsBackEndHelloworld
+module RailsReactHelloworld
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.api_only = true
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
